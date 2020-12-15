2f7eab2b49ca7505c050e020888e611ea6127b02
Mark Rebuck <Mark.Rebuck+github@gmail.com>
2020-12-15 01:05:30 -0500

    Clean up code to remove boilerplate

--- day15.py
+++ day15.py
@@ -1,24 +1,5 @@
-import re
-import os
-import time
-
 with open('input', 'r') as file:
-    lines = [line.strip() for line in file if line.strip()]
-    words = [line.split() for line in lines]
-    board = [list(line.strip()) for line in lines]
-    board_flat = sum(board, [])
-numbers = []
-try:
-    with open('input', 'r') as file:
-        numbers = [int(n) for n in file.read().replace(',', ' ').split()]
-except:
-    pass
-            
-print(f"lines: {lines}")
-print(f"words: {words}")
-print(f"board: {board}")
-print(f"board_flat: {board_flat}")
-print(f"numbers: {numbers}")
+    numbers = [int(n) for n in file.read().replace(',', ' ').split()]
 
 spoken = 0
 lasts = [[0] for _ in range(30000000)]
@@ -28,13 +9,7 @@ for i in range(30000000):
         lasts[n].append(i+1)
         spoken = n
     else:
-        #print(f"spoken = {spoken}")
         last = lasts[spoken]
-        if len(last) < 3:
-            lasts[0].append(i+1)
-            spoken = 0
-        else:
-            spoken = last[-1] - last[-2]
-            lasts[spoken].append(i+1)
-    #print(spoken)
-print(spoken)    
+        spoken = 0 if len(last) < 3 else last[-1] - last[-2]
+        lasts[spoken].append(i+1)
+print(spoken)
